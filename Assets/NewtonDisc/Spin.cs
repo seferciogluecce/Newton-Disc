4564113a8646d490fd7c45f6b228dfcb95591787
Ece Sefercioğlu <seferciogluecce@users.noreply.github.com>
2019-10-13 21:58:03 +0300

    Created expamle discs, code changes

--- Assets/NewtonDisc/Spin.cs
+++ Assets/NewtonDisc/Spin.cs
@@ -1,17 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class DoAFlips : MonoBehaviour
+public class Spin : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     Vector3 MouseStart;
     Vector3 MouseEnd;
-    public float forceConstant = 5;
+    public float forceMultiplier = 12;
     public int maxAngularVelocity = 40;
     int rotationDirection;
+
     Rigidbody rb;
     Material NewtonDiscEffectMat;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -24,10 +24,8 @@ public class DoAFlips : MonoBehaviour
 
         if(rb.angularVelocity.magnitude > 0)
         {
-
             float ratio = rb.angularVelocity.magnitude / maxAngularVelocity;
             NewtonDiscEffectMat.SetFloat("_Angle", ratio*360*rotationDirection);
-
         }
 
         if (Input.GetMouseButtonDown(0))
@@ -35,16 +33,12 @@ public class DoAFlips : MonoBehaviour
             MouseStart = Input.mousePosition;
         }
 
-
         if (Input.GetMouseButtonUp(0))
         {
             MouseEnd = Input.mousePosition;
-
             float distance = Vector3.Distance(MouseEnd, MouseStart);
             rotationDirection = (MouseEnd.x>MouseStart.x) ? -1 : 1;
-            rb.AddTorque(transform.up * distance * forceConstant * rotationDirection);
-
-
+            rb.AddTorque(transform.up * distance * forceMultiplier * rotationDirection);
         }
 
     }
